fe7472f9430879bd703bf3d28029d0359ad708be
Aaron <aaron.hernandez85@gmail.com>
2021-12-03 23:52:23 +0100

    Funciones Básicas

--- p1.py
+++ p1.py
@@ -5,4 +5,12 @@ def sumaTodos(limitTo):
         
     return resultado
 
+def sumaTodosLoscuadrados(limitTo):
+    resultado = 0
+    for i in range(limitTo+1):
+        resultado += i*i
+        
+        return resultado
+    
 print(sumaTodos(100))
+print(sumaTodosLoscuadrados(3))
